6d2d7ff19cb1c1f2c4fbd3487a0d693318dcfe3c
Everglow159 <378367643@qq.com>
2022-11-07 20:00:03 +0800

    2022/11/7 19:59

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	//only one main for one project
+	//This is web project demo
+	server := http.Server{
+		Addr: "127.0.0.1:8080",
+	}
+	if err := server.ListenAndServe(); err != nil {
+		log.Println(err)
+	}
+}
